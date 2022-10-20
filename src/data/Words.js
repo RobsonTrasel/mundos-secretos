9a24373533e757c1e85533dadb7ac0a9fec06461
RobsonTrasel <robson.trasel@gmail.com>
2022-10-20 00:41:57 -0300

    align items

---
+++ src/data/Words.js
@@ -0,0 +1,20 @@
+export const Wordlist = {
+    carro: [
+        "Motor", "Porta", "Capo", "Pneu", "Lanterna"
+    ],
+    fruta: [
+        "Banana", "Pera", "Maçã", "Uva", "Laranja"
+    ],
+    corpo: [
+        "Mao", "Pé", "Braco", "Nariz", "Costas"
+    ],
+    computador: [
+        "RAM", "GPU", "Processador", "USB", "HDMI"
+    ],
+    programacao: [
+        "Javascript", "Typescript", "React", "Redux", "Laravel"
+    ],
+    alimento: [
+        "Arroz", "Feijão", "Carne", "Cebola", "Salada"
+    ]
+}
